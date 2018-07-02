33faa662022d5e3d1e7acaa721d81328b85e54dd
j <jawad.bouali@hotmail.Fr>
2018-07-02 16:02:58 +0200

    variable nom

--- mon-projet/src/js/style.js
+++ mon-projet/src/js/style.js
@@ -11,14 +11,19 @@ var mage ={
     age : 110,
     taille : 1.80,
     arme : "Épée D'argent",
-    attaque : 15
+    attaque : 15,
+    vie : 30
 }
 var paysan ={
     nom : "Arthur",
     age : 30 ,
     taille : 1.70,
     arme : "Faucille",
-    attaque : 7
+    attaque : 7,
+    vie : 5
 }
 var nom_guerrier = guerrier.nom;
-var nom_mage = 
+var nom_mage = mage.nom;
+var nom_paysan = paysan.nom;
+
+
