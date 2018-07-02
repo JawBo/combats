d166dbeda6083de08d4d9727d96aa066e5e3c8aa
j <jawad.bouali@hotmail.Fr>
2018-07-02 16:00:55 +0200

    Ajout des persos

---
+++ mon-projet/src/js/style.js
@@ -0,0 +1,24 @@
+var guerrier ={
+    nom : "Kratos",
+    age : 127, 
+    taille : 1.88,
+    arme : "lames enchainées",
+    attaque : 20,
+    vie : 50
+}
+var mage ={
+    nom: "Geralt",
+    age : 110,
+    taille : 1.80,
+    arme : "Épée D'argent",
+    attaque : 15
+}
+var paysan ={
+    nom : "Arthur",
+    age : 30 ,
+    taille : 1.70,
+    arme : "Faucille",
+    attaque : 7
+}
+var nom_guerrier = guerrier.nom;
+var nom_mage = 
